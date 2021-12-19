37780f5d066ae23fc599f70aa69de0cac41556e2
taylor.holmes <taylorh2343@gmail.com>
2021-12-19 17:16:31 -0600

    code for workday scheduler

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    var currentDay = $("#currentDay");
+    currentDay.text(moment().format("dddd, MMMM Do"));
+
+    var saveButton = $(".saveBtn").on("click",function(){
+        console.log($(this))
+
+        var currentBtn = $(this);
+
+        var descriptionDiv = $(this).siblings('.description');
+        var descriptionText = descriptionDiv.val();
+
+        var hourId = currentBtn.parent().attr('id');
+
+        localStorage.setItem(hourId, descriptionText);
+
+    });
+
+    function hourHandler (){
+        var currentHour = moment().hours();
+
+        $(".time-block").each(function(index){
+            console.log(index, $(this))
+
+            var block = $(this);
+            var blockId = block.attr("id");
+            var blockHour = blockId.split("-")[1];
+
+            console.log(blockHour)
+
+            if (currentHour == blockHour) {
+                $(this).addClass("present");
+            } 
+            else if (blockHour < currentHour){
+                $(this).addClass("past");
+            }
+             else if (blockHour > currentHour){
+                $(this).addClass("future");
+            }
+        });
+    };
+
+    hourHandler();
+
+});
